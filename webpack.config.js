814539ab5658a60c01d06ae12d3a9e9f09feb670
Игорь Алексеевич <webpr@list.ru>
2026-02-11 22:10:40 +1000

    Устанавливает пакеты, до .gitignore

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: './src/main.js',   // main.js - точка входа
+  output: {
+    filename: 'bundle,js',  // файл сборки (бандл)
+
+    // получить абсолютный путь с помощью path.resolve
+    path: path.resolve(__dirname, 'build'), // по какому пути наход. bundle,js
+
+    clean: true,            //  очистка директории перед новой сборкой
+  },
+  devtool: 'source-map',  // для просмотра исходного кода
+
+  plugins: [
+    // создается экземпляр плагина CopyPlugin, позволяет выполнять копирование файлов в указанную папку
+    new CopyPlugin({
+      patterns: [{ from: 'public'}]
+    }),
+  ],
+
+};
