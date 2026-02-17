1c69d539aa490b1b8c61d114cbe676310015539c
Игорь Алексеевич <webpr@list.ru>
2026-02-18 06:37:38 +1000

    Делает Loading и меню

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 
-import NewTaskButtonView from './view/menu-button.js';
+import NewTaskButtonView from './view/menu.js';
+import NewTaskLoadingView from './view/loading.js';
 import {render} from './render.js';
 
 const tripMain = document.querySelector('.trip-main');
 const tripControlsFilters = tripMain.querySelector('.trip-controls__filters');
 
+const pageMain = document.querySelector('.page-main');
+const tripEvents = pageMain.querySelector('.trip-events');
+
 render(new NewTaskButtonView(), tripControlsFilters);
+render(new NewTaskLoadingView(), tripEvents);
