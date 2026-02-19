14665b8d1a6c1ee5d04fca8840446d42bb7394a4
Игорь Алексеевич <webpr@list.ru>
2026-02-19 17:24:34 +1000

    Делает фильтр и сортировку

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
-
-import NewTaskButtonView from './view/menu.js';
-import NewTaskLoadingView from './view/loading.js';
 import {render} from './render.js';
+
+import NewTaskButtonView from './view/filter.js';
+import NewTaskSortingView from './view/sorting.js';
+//import NewTaskLoadingView from './view/loading.js';
+
 
 const tripMain = document.querySelector('.trip-main');
 const tripControlsFilters = tripMain.querySelector('.trip-controls__filters');
 
 const pageMain = document.querySelector('.page-main');
-const tripEvents = pageMain.querySelector('.trip-events');
+//const tripEvents = pageMain.querySelector('.trip-events');
+
+const tripSort = pageMain.querySelector('.trip-events');
 
 render(new NewTaskButtonView(), tripControlsFilters);
-render(new NewTaskLoadingView(), tripEvents);
+//render(new NewTaskLoadingView(), tripEvents);
+
+render(new NewTaskSortingView(), tripSort);
